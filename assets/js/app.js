dbe9b52aa8f183fc023c920f6d790a429fc471f8
Gabriel Alvarez <jgabriel96@hotmail.com>
2025-05-26 11:18:15 -0600

    fixed: Se creo codigo para obtener la informacio de la api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,42 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+/* Se cambio por get element by id  */
+const $n = document.getElementById('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-/* Se cambio la funcion sincrona a una funcion asincrona debido a que esta utilizando un await  */
-async function displayUser(username) {
+/* Se cambio la funcion sincrona a una funcion asincrona debido a que esta utilizando un await, se cambio nombre de la funcion */
+async function getData(apiLink) {
+  try{
+    const response = await fetch(apiLink);
+    console.log(response);
+    const datosApi = await response.json();
+    console.log(datosApi);
+    return datosApi;
+  } catch (error){
+    handleError(error);
+  }
+}
+
+function handleError(err) {
+  console.log('OH NO!');
+  console.log(err);
+  //No tenia asiganda correctamente la variable 
+  $n.textContent = `Algo salió mal: ${err}`
+}
+
+const insertInDom = () => {
+
+}
+
+const createInDom = async (apiLink) =>{
+  const data = await getData(apiLink);
+  const username = data.results;
+  console.log(username);
+  
+}
+
+/* async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
@@ -17,7 +48,8 @@ async function displayUser(username) {
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  //No tenia asiganda correctamente la variable 
+  $n.textContent = `Algo salió mal: ${err}`
 }
-
-displayUser('stolinski').catch(handleError);
+ */
+createInDom(baseEndpoint);
